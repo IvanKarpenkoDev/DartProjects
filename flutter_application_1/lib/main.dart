39714a48ada2b9293162b4bc6d3e05c2c3e9a36a
ivan <ivan.karpenko.dev@gmail.com>
2023-09-13 13:23:47 +0300

    update secondApp

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,12 +9,53 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+    return MaterialApp(
+      title: 'Test',
+      theme: ThemeData.dark(),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('asd'),
+        foregroundColor: const Color.fromRGBO(245, 129, 255, 1),
+        iconTheme: const IconThemeData(),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              color: const Color.fromRGBO(245, 129, 255, 1),
+              onPressed: () {
+                print("object");
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
         ),
       ),
+      body: SafeArea(
+          child: Center(
+              child: Container(
+                  height: 200,
+                  width: 200,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 85, 85, 85),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text(
+                    "asd",
+                    style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.w500,
+                        color: Color.fromRGBO(245, 129, 255, 1)),
+                  )))),
     );
   }
 }
